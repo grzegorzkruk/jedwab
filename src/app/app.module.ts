dbe9759c0bba49ea7b5c91fab15925e5a62a76f3
Grzegorz Kruk <poczta@grzegorzkruk.pl>
2017-08-22 14:15:42 +0200

    Added navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LogoComponent } from './logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule
